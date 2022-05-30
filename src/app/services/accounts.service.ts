f7f5414ad26f71742ec884f2612bd9e49714756e
MB-cyber <mouhssine.bentabet@gmail.com>
2022-05-31 00:29:35 +0100

    c/d/T operations

--- src/app/services/accounts.service.ts
+++ src/app/services/accounts.service.ts
@@ -15,4 +15,17 @@ export class AccountsService {
   public getAccount(accountId : string, page : number, size : number):Observable<AccountDetails>{
     return this.http.get<AccountDetails>(environment.backendhots+"/accounts/"+accountId+"/pageOperations?page="+page+"&size="+size);
   }
+  public debit(accountId : string, amount : number, description:string){
+    let data={accountId : accountId, amount : amount, description : description}
+    return this.http.post(environment.backendhots+"/accounts/debit",data);
+  }
+  public credit(accountId : string, amount : number, description:string){
+    let data={accountId : accountId, amount : amount, description : description}
+    return this.http.post(environment.backendhots+"/accounts/credit",data);
+  }
+  public transfer(accountSource: string,accountDestination: string, amount : number, description:string){
+    let data={accountSource, accountDestination, amount, description }
+    return this.http.post(environment.backendhots+"/accounts/transfer",data);
+  }
+
 }
